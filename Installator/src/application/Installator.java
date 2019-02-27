e5576669e1789356b7c1e2e9cb24f7adcd20f33a
best2327 <unconfigured@null.spigotmc.org>
2019-02-27 20:25:53 +0100

    Update 0.01
    Added Everything basically

---
+++ Installator/src/application/Installator.java
@@ -0,0 +1,80 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class Installator extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+			Group root = new Group();
+			ObservableList<Node> list = root.getChildren();
+			Scene scene = new Scene(root,750,450);
+			
+			Line border = new Line();
+			border.setStartX(0);
+			border.setEndX(750);
+			border.setStartY(30);
+			border.setEndY(30);
+			border.setOpacity(0.125);
+			
+			ObservableList<String> options = 
+				    FXCollections.observableArrayList(
+				        "Initiative Mod",
+				        "MCReconfigured"
+				    );
+			ComboBox<String> comboBox = new ComboBox<String>();
+			comboBox.setItems(options);
+			comboBox.setPrefSize(130, 25);
+			comboBox.setLayoutX(215);
+			comboBox.setLayoutY(225);
+			
+			Button install = new Button();
+			install.setText("Install");
+			install.setLayoutX(375);
+			install.setLayoutY(225);
+			install.setPrefSize(50, 25);
+			
+			Text title = new Text();
+			title.setText("Installator");
+			title.setStroke(Color.BLACK);
+			title.setStrokeWidth(0.75);
+			title.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 16));
+			title.setX(10);
+			title.setY(25);
+			
+			list.add(title);
+			list.add(border);
+			list.add(comboBox);
+			list.add(install);
+			
+			primaryStage.initStyle(StageStyle.UNDECORATED);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
